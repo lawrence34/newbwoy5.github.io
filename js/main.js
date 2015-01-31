dcb1e84d841520ce5cf929356de62e154278d71e
hello world <kufogbemawuli1@gmail.com>
2015-01-31 10:37:42 +0000

    Simple Flat Design

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   
+    $('img').hover(function() {
+        $(this).fadeTo('normal',.5);
+    }, 
+      function() {
+        $(this).fadeTo('normal',.9);
+    });
+    
+});
+
+ 
