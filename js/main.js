07716a7193b3895fe2a4e4b772bc8e2fccbccd5d
hello world <kufogbemawuli1@gmail.com>
2015-02-02 14:07:59 +0000

    Simple Flat Design

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
    
-    $('img').hover(function() {
+    $('img:not(#author1)').hover(function() {
         $(this).fadeTo('normal',.5);
     }, 
       function() {
         $(this).fadeTo('normal',.9);
     });
+    
+    TweenMax.staggerFrom('img',0.5,{opacity:0,y:200,delay:0.5},0.2);
+    
     
 });
 
